beec1509d403b301acdf10e373ab106270813b0d
hartred <artiomse@gmail.com>
2019-09-22 18:00:45 +0300

    Added the launch method implementation

--- client.py
+++ client.py
@@ -1,15 +1,29 @@
+import requests
 from typing import List
+
 
 class BaseClient:
     
-    def __init__(self):
-        pass
+    def __init__(self, server_address):
+        self.server_address = server_address
+        self.userlist = None
+        self.nickname = None
 
-    def launch(self):
-        pass
-        
-    def set_nickname(self, nickname: str):
-        pass
+    def launch(self) -> bool:
+        try:
+            ping_resp = requests.get(f'{self.server_address}/ping')
+            ping_resp_json = ping_resp.json()
+
+            if not ping_resp_json:
+                return False
+            
+            server_status = ping_resp_json.get('status') == 'OK'
+            return server_status
+
+        except Exception as exc:
+            print('exception occurred:', exc)
+
+        return False
 
     def get_userlist(self) -> List[str]:
         pass
@@ -23,9 +37,29 @@ class BaseClient:
     def exit(self):
         pass
 
+    def print_userlist(self):
+        print('Users in the chat:')
+        
+        for user in self.userlist:
+            print(f'- {user}')
 
 def main():
-    pass
+    client = BaseClient(
+        server_address='http://127.0.0.1'
+    )
+
+    if not client.launch():
+        print('Client launch failed! Aborting.')
+        return
+
+    nickname = input('Enter nickname: ')
+    client.nickname = nickname
+    print(client.nickname)
+
+    #user_list = client_obj.get_userlist()
+
+
+
 
 if __name__ == '__main__':
     main()
